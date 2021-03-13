6183fa39da117f9db6b57399c1a0994dddc3c3d6
KirillReal <kiril5471710@gmail.com>
2021-03-13 03:54:20 +0300

    1.2. Режим ожидания. [#490685]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -5,7 +5,7 @@ public class Wget {
         Thread load = new Thread(
                 () -> {
                     try {
-                        for(int i = 0; i < 101;i++) {
+                        for (int i = 0; i < 101; i++) {
                             System.out.print("\rLoading : " + i  + "%");
                             Thread.sleep(1000);
                         }
