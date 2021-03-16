4a5d215cae7d31166ce525939ba1499850ad2841
KirillReal <kiril5471710@gmail.com>
2021-03-16 23:59:30 +0300

    1. Visibility. Общий ресурс вне критической секции [#490698]

---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,44 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class ParseFile {
+    private File file;
+
+    public synchronized void setFile(File f) {
+        file = f;
+    }
+
+    public synchronized File getFile() {
+        return file;
+    }
+
+    public synchronized String getContent() throws IOException {
+        StringBuilder output = new StringBuilder();
+        try (BufferedReader i = new BufferedReader(new FileReader(file))) {
+            i.lines().forEach(output::append);
+        }
+        return output.toString();
+    }
+
+    public synchronized String getContentWithoutUnicode() throws IOException {
+        StringBuilder output = new StringBuilder();
+        try (BufferedReader i = new BufferedReader(new FileReader(file))) {
+            int data;
+            while ((data = i.read()) > 0) {
+                if (data < 0x80) {
+                    output.append((char) data);
+                }
+
+            }
+        }
+        return output.toString();
+    }
+
+    public synchronized void saveContent(String content) throws IOException {
+        try (PrintWriter o = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file))))
+        {
+            o.write(content);
+        }
+    }
+}
