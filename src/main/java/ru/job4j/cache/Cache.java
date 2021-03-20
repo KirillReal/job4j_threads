2cb7cd804d9157a23fd2c867073fa6d2540ef1d2
KirillReal <kiril5471710@gmail.com>
2021-03-21 01:38:39 +0300

    1. Неблокирующий кеш [#490705]

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,28 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public void update(Base model) {
+        memory.computeIfPresent(model.getId(), (id, base) -> {
+                    if (base.getVersion() != model.getVersion()) {
+                        throw new OptimisticException("версии не совпали");
+                    }
+                    base = new Base(id, model.getVersion() + 1);
+                    base.setName(model.getName());
+                    return base;
+                }
+        );
+    }
+
+    public void delete(Base model) {
+      memory.remove(model.getId());
+    }
+}
